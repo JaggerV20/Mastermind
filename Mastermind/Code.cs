c8e6d9d27a22ea62724af3266a1a33d427a362a7
JaggerV20 <cenobia620@gmail.com>
2024-08-14 16:09:46 -0500

    Created "Code" class Code class contains a 4 "peg" code represented as ints and an array with the number of each color in the code. It also contains an array for int to color string conversion. "Code" could possibly be a struct, but I will see if it needs additional methods and additional rules about structs.

---
+++ Mastermind/Code.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind
+{
+    internal class Code
+    {
+        //Fields
+        private static string[] pegColors = { "Red", "Orange", "Yellow", "Green", "Blue", "Purple" };
+        public int[] colorCode = new int[4];
+        public int[] ColorCode
+        {
+            get { return colorCode; }
+            private set { colorCode = value; }
+        }
+        public int[] colorCount = new int[6];
+        public int[] ColorCount
+        {
+            get { return colorCount; }
+        }
+
+        public Code(int[] colorCode)
+        {
+            this.ColorCode = colorCode;
+            for (int i = 0; i < colorCode.Length; i++)
+            {
+                ++colorCount[colorCode[i]];
+            }
+        }
+    }
+}
