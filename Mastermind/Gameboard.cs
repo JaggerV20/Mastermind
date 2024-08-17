9bd66b859990a409298e9f5b3c7afa06acccc1ec
JaggerV20 <cenobia620@gmail.com>
2024-08-16 20:12:34 -0500

    Created fields and constructors of "Gameboard" Gameboard will contain a "Board" of "Codes" and a MasterCode. Player wins if they can guess the MasterCode before the "Board" is full, as indicated by "GuessesRemaining".

--- Mastermind/Gameboard.cs
+++ Mastermind/Gameboard.cs
@@ -8,5 +8,37 @@ namespace Mastermind
 {
     internal class Gameboard
     {
+        //Fields
+        const int NORMALGAMELENGTH = 12;
+        public Code[] Board
+        {
+            get; private set;
+        }
+        public int GuessesRemaining
+        {
+            get; private set;
+        }
+        public Code MasterCode
+        {
+            get; private set;
+        }
+
+        //Constructor
+        public Gameboard()
+        {
+            Board = new Code[NORMALGAMELENGTH];
+            GuessesRemaining = NORMALGAMELENGTH;
+            //Generate MasterCode
+            int[] GeneratedCode = new int[4];
+            Random rand = new Random();
+            for(int i = 0; i < GeneratedCode.Length; i++)
+            {
+                GeneratedCode[i] = rand.Next(0, 6);
+            }
+            MasterCode = new Code(GeneratedCode);
+        }
+
+        //Methods
+
     }
 }
