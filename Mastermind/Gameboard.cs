f0c76da69e423f7583eafe88f48060884e3fe833
JaggerV20 <cenobia620@gmail.com>
2024-08-19 13:37:01 -0500

    Created templates for game methods Added a GuessAnalysis int[][] for the purpose of holding all guess analysis alongside the game board

--- Mastermind/Gameboard.cs
+++ Mastermind/Gameboard.cs
@@ -14,6 +14,12 @@ namespace Mastermind
         {
             get; private set;
         }
+        //Format : [Correct, Wrong Place, Completely Wrong]
+        public int[][] GuessAnalysis
+        {
+            get;
+            private set;
+        }
         public int GuessesRemaining
         {
             get; private set;
@@ -27,11 +33,23 @@ namespace Mastermind
         public Gameboard()
         {
             Board = new Code[NORMALGAMELENGTH];
+            GuessAnalysis = new int[3];
             GuessesRemaining = NORMALGAMELENGTH;
 
         }
 
         //Methods
+        /*
+         * Returns true to end game
+         * Game loop method that calls other methods to run the game.
+         * Main method won't do much except possibly selecting a game mode.
+         * Generate a master code, prompt the user for guesses, analyze guesses,
+         * and return results. Provide a visual representation of the game state
+         */
+        public bool GameLoop()
+        {
+            return true;
+        }
 
         //Return an int array to be used in a Code constructor
         private int[] GenerateCode()
@@ -43,6 +61,53 @@ namespace Mastermind
                 GeneratedCode[i] = rand.Next(0, 6);
             }
             return GeneratedCode;
+        }
+        /*
+         * Prompt the player to enter a guess. 
+         * Allow the user to select a hole/index and select a color.
+         * When guess is valid and player is satisfied, they can enter an escape value to end
+         * and return a new Code
+         */
+        private Code GetGuess()
+        {
+            int[] filler = { 0, 0, 0, 0 };
+            return new Code(filler);
+        }
+
+        /*
+         * Take the entered Code to  return an array for GuessAnalysis
+         * Return format is a size 3 array representing
+         * [Correct placements, Correct colors in wrong places, Completely wrong placements]
+         */
+        private int[] GetFeedback(Code guess)
+        {
+            int[] info = new int[3];
+            return info;
+        }
+
+        /*
+         * Display the game board using Board, GuessAnalysis, and MasterCode
+         * Format Idea:
+         * R . . . . . . . . . . . ?
+         * R . . . . . . . . . . . ?
+         * R . . . . . . . . . . . ?
+         * R . . . . . . . . . . . ?
+         * 
+         * W 
+         * R
+         * .
+         * .
+         * 
+         * Top - Gameboard showing guesses. '?' represents unknown master code.
+         * Shown at end. Letters are colors
+         * Bottom - Feedback of the code above. 
+         * W - Completely correct
+         * R - Right color, wrong spot
+         * . - Incorrect
+         */
+        private void DisplayBoard()
+        {
+
         }
 
     }
